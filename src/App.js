c4f34eddae7cb012125cb5e5f7dfd5c003ebd50f
Kamva-pro <kamvelihlefatman@gmail.com>
2025-02-17 19:05:33 +0200

    seperation of concerns

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
-import CardDetails from './CardDetails.jsx';
-import Explore from './Explore.jsx';
-import HomePage from './HomePage.jsx';
-import ReactDOM from "react-dom/client";
+import CardDetails from './pages/CardDetails.jsx';
+import HomePage from './pages/HomePage.jsx';
+import Explore from './pages/Explore.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Blog from './pages/Blog.jsx';
+import Gallery from './pages/Gallery.jsx';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Trip-tips">
       <Routes >
         <Route path='/' element={<HomePage/>}/>
         <Route path='details' element={<CardDetails/>}/>
         <Route path='explore' element={<Explore/>}/>
+        <Route path='blog' element={<Blog/>}/>
+        <Route path='gallery' element={<Gallery/>}/>
       </Routes>
     </BrowserRouter>
   );
