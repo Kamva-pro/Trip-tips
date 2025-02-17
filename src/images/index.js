3fe6d03e07db207a52b3a6107733ea10cf373e2b
Kamva-pro <kamvelihlefatman@gmail.com>
2025-02-17 20:06:10 +0200

    exported gallery images

--- src/images/index.js
+++ src/images/index.js
@@ -7,22 +7,88 @@ import slider3 from "../images/slider-img3.jpg"
 import india from "../images/p5.jpg"
 import kenya from "../images/kenya-4.jpg"
 import bali from "../images/bali-2.jpg"
+import bali_2 from "../images/bali.jpg"
+import banner1 from "../images/banner-1.jpg"
+import banner2 from "../images/banner-2.jpg"
+import banner3 from "../images/banner-3.jpg"
+import bg from "../images/bg.jpg"
+import egypt from "../images/egypt-1.jpg"
+import florence from "../images/florence.jpg"
+import gallery2 from "../images/gallery-2.jpg"
+import gallery9 from "../images/gallery-9.jpg"
+import gallery2 from "../images/gallery-14.jpg"
+import hippo from "../images/hippo.jpg"
+import iceland from "../images/iceland.jpg"
+import img1 from "../images/img-1.jpg"
+import index06 from "../images/index-06.jpg"
+import index07 from "../images/index-07.jpg"
+import index08 from "../images/index-08.jpg"
+import index09 from "../images/index-09.jpg"
+import index10 from "../images/index-10.jpg"
+import indonesia from "../images/indonesia.jpg"
 import carribean from "../images/carribean.jpg"
 import tokyo from "../images/tokyo.jpg"
+import kenya2 from "../images/kenya-1.jpg"
+import kenya3 from "../images/kenya-3.jpg"
+import p1 from "../images/p1.jpg"
+import p2 from "../images/p2.jpg"
+import paris from "../images/paris.jpg"
+
+import pex1 from "../images/pexels-andy-vu-3217911.jpg"
+import pex2 from "../images/pexels-blue-arauz-14531217.jpg"
+import pex3 from "../images/pexels-gashif-rheza-3262554.jpg"
+import pex4 from "../images/pexels-nataliya-vaitkevich-5291520.jpg"
+import pex5 from "../images/pexels-rosivan-morais-3698913.jpg"
+import pex6 from "../images/pexels-suliman-sallehi-1822458.jpg"
+import pex7 from "../images/pexels-tima-miroshnichenko-7009581.jpg"
+import pex8 from "../images/pexels-tnarg-4625371.jpg"
+
 import menu_white from "../images/menu-white.png";
 import menu_dark from '../images/menu-dark.png';
+
 
 export {
     aaron,
     about,
     babylon,
     bali,
+    bali_2,
+    banner1,
+    banner2,
+    banner3,
+    bg,
+    carribean,
+    egypt,
+    florence,
+    gallery2,
+    gallery9,
+    hippo,
+    iceland,
+    img1,
+    indonesia,
+    index06,
+    index07,
+    index08,
+    index09,
+    index10,
+    india,
+    kenya,
+    kenya2,
+    kenya3,
+    p1,
+    p2,
+    paris,
+    pex1,
+    pex2,
+    pex3,
+    pex4,
+    pex5,
+    pex6,
+    pex7,
+    pex8,
     slider,
     slider2,
     slider3,
-    india,
-    kenya,
-    carribean,
     tokyo,
     menu_dark,
     menu_white
